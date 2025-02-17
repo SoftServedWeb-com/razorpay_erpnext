4b42717a20508af8c052a6df3ae5d7b3895dbc4a
SSW External hdd <softservedweb@gmail.com>
2025-02-17 10:24:18 +0530

    added try catch statement to the super function call

--- razorpay_erpnext/razorpay_erpnext/custom_doctype/sales_invoice.py
+++ razorpay_erpnext/razorpay_erpnext/custom_doctype/sales_invoice.py
@@ -3,7 +3,10 @@ from healthcare.healthcare.custom_doctype.sales_invoice import HealthcareSalesIn
 # Override the Sales Invoice class from healthcare module (since the healthcare already has a Sales Invoice Override class)
 class RazorpaySalesInvoice(HealthcareSalesInvoice):
     def after_insert(self):
-        super().after_insert() # Call the existing after_insert method from the parent class
+        try:
+            super().after_insert() # Call the existing after_insert method from the parent class
+        except:
+            pass
         print("RazorpaySalesInvoice after_insert called")
     def create_payment_request(self):
         pass
